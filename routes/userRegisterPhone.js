225b4d8e3036e863ef3b971329469a21fcc57b01
Roman <roman@macbook.local>
2026-04-19 19:48:36 +0300

    Delete phone login for user

--- routes/userRegisterPhone.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { registerPhone: userRegisterPhone } = require('../controllers/userRegisterPhoneController');
-
-router.post('/register-phone', userRegisterPhone );
-
-module.exports = router;
